e479e85e149e73965a9cba226844a576abdf4cc1
Derek <dtlafever@gwu.edu>
2019-05-16 15:55:23 -0400

    Update gameHook.py

--- gameHook.py
+++ gameHook.py
@@ -1,2 +1,13 @@
 # Test trying to connect to an already started game
 
+from pysc2 import run_configs
+from pysc2.lib import features
+from pysc2.lib import point
+from pysc2.lib import point_flag
+from pysc2.lib import protocol
+from pysc2.lib import remote_controller
+
+from pysc2.lib import gfile
+from s2clientprotocol import common_pb2 as sc_common
+from s2clientprotocol import sc2api_pb2 as sc_pb
+
